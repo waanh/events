227da43e6b08062d07e71f83fe716a0bbaff2c29
waanh <anhimovalera@gmail.com>
2024-11-27 21:40:24 +0300

    8

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
