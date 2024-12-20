28e73bdcb60265e2115af9294096e2b20cc7e00e
waanh <anhimovalera@gmail.com>
2024-12-20 17:30:25 +0300

    014

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ASSET_PATH = process.env.CI ? '' : './'; 
+const ASSET_PATH = process.env.NODE_ENV === 'production' ? '/events/' : '/';
 
 module.exports = {
   entry: './src/index.js',
